e20730067df8c06e44d6cb09215289e2cd88de97
Wlad <schumacherwlad@gmail.com>
2023-11-15 20:19:04 +0100

    Changed to model view controller template

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cloud-notes
+
+
+docker run --cap-add SYS_PTRACE -e 'ACCEPT_EULA=1' -e 'MSSQL_SA_PASSWORD=cool123 -p 1433:1433 --name cloud-notes-db -d mcr.microsoft.com/azure-sql-edge
---
+++ cloud-notes-webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cloud_notes_webapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
